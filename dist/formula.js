177d79ff82858d65a808741af8525321e323ac6a
Yung729 <leongkahyung@gmail.com>
2023-12-08 17:08:24 +0800

    Update formula.js

--- dist/formula.js
+++ dist/formula.js
@@ -48,20 +48,9 @@ function calculateStampDuty2() {
     let purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
     let financeMargin = parseFloat(document.getElementById("financeMargin").value);
     let stampDuty = Math.ceil(purchasePrice / 1000) * 1000 * financeMargin / 100;
-    if (stampDuty >= 100000) {
-        if (500000 < stampDuty && stampDuty <= 1000000) {
-            stampDuty = 100000 * 0.01 + 400000 * 0.02 + (stampDuty - 500000) * 0.03;
-        }
-        else if (stampDuty > 1000000) {
-            stampDuty = 100000 * 0.01 + 400000 * 0.02 + 500000 * 0.03 + (stampDuty - 1000000) * 0.04;
-        } else {
-            stampDuty = 100000 * 0.01 + (stampDuty - 100000) * 0.02;
-        }
-    } else {
-        stampDuty = stampDuty * 0.01;
-    }
+    
 
-    return stampDuty;
+    return stampDuty * 0.005 ;
 }
 
 function updateDOM() {
