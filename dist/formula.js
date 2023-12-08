1921e95ca17b9a6cff7912176caf3060213a8df8
Yung729 <leongkahyung@gmail.com>
2023-12-08 17:01:47 +0800

    a

--- dist/formula.js
+++ dist/formula.js
@@ -1,6 +1,5 @@
 function calculateLegalFee1() {
     let purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
-    let financeMargin = parseFloat(document.getElementById("financeMargin").value);
     let legalFee = Math.ceil(purchasePrice / 1000) * 1000;
     if (legalFee <= 500000) {
         legalFee = legalFee * 1.25 / 100;
@@ -26,25 +25,23 @@ function calculateLegalFee2() {
 
 function calculateStampDuty1() {
     let purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
-    let financeMargin = parseFloat(document.getElementById("financeMargin").value);
     let stampDuty = Math.ceil(purchasePrice / 1000) * 1000;
+    
     if (stampDuty >= 100000) {
-        if (101000 < stampDuty && stampDuty < 500000) {
-            if (501000 < stampDuty && stampDuty < 1000000) {
-                if (stampDuty > 1000000) {
-                    stampDuty = 100000 * 1 / 100 + 500000 * 2 / 100 + 1000000 * 3 / 100 + (stampDuty - 1000000) * 4 / 100;
-                } else {
-                    stampDuty = 100000 * 1 / 100 + 500000 * 2 / 100 + (stampDuty - 500000) * 3 / 100;
-                }
-            } else {
-                stampDuty = 100000 * 1 / 100 + (stampDuty - 100000) * 2 / 100;
-            }
-        } else {
-            stampDuty = 0;
+        if (500000 < stampDuty && stampDuty <= 1000000) {
+            stampDuty = 100000 * 0.01 + 400000 * 0.02 + (stampDuty - 500000) * 0.03;
         }
-    }
+        else if (stampDuty > 1000000) {
+            stampDuty = 100000 * 0.01 + 400000 * 0.02 + 500000 * 0.03 + (stampDuty - 1000000) * 0.04;
+        } else {
+            stampDuty = 100000 * 0.01 + (stampDuty - 100000) * 0.02;
+        }
+    } else {
+        stampDuty = stampDuty * 0.01;
+    }
 
     return stampDuty;
+
 }
 
 function calculateStampDuty2() {
@@ -52,28 +49,25 @@ function calculateStampDuty2() {
     let financeMargin = parseFloat(document.getElementById("financeMargin").value);
     let stampDuty = Math.ceil(purchasePrice / 1000) * 1000 * financeMargin / 100;
     if (stampDuty >= 100000) {
-        if (101000 < stampDuty && stampDuty < 500000) {
-            if (501000 < stampDuty && stampDuty < 1000000) {
-                if (stampDuty > 1000000) {
-                    stampDuty = 100000 * 1 / 100 + 500000 * 2 / 100 + 1000000 * 3 / 100 + (stampDuty - 1000000) * 4 / 100;
-                } else {
-                    stampDuty = 100000 * 1 / 100 + 500000 * 2 / 100 + (stampDuty - 500000) * 3 / 100;
-                }
-            } else {
-                stampDuty = 100000 * 1 / 100 + (stampDuty - 100000) * 2 / 100;
-            }
-        } else {
-            stampDuty = 0;
+        if (500000 < stampDuty && stampDuty <= 1000000) {
+            stampDuty = 100000 * 0.01 + 400000 * 0.02 + (stampDuty - 500000) * 0.03;
         }
-    }
+        else if (stampDuty > 1000000) {
+            stampDuty = 100000 * 0.01 + 400000 * 0.02 + 500000 * 0.03 + (stampDuty - 1000000) * 0.04;
+        } else {
+            stampDuty = 100000 * 0.01 + (stampDuty - 100000) * 0.02;
+        }
+    } else {
+        stampDuty = stampDuty * 0.01;
+    }
 
     return stampDuty;
 }
 
 function updateDOM() {
     document.getElementById("value1").innerHTML = calculateLegalFee1();
-    document.getElementById("value2").innerHTML = calculateLegalFee2();
-    document.getElementById("value3").innerHTML = calculateStampDuty1();
+    document.getElementById("value2").innerHTML = calculateStampDuty1();
+    document.getElementById("value3").innerHTML = calculateLegalFee2();
     document.getElementById("value4").innerHTML = calculateStampDuty2();
 }
 
