ac49bb28fd62391f35f677f4a335a56261974f8e
Yung729 <leongkahyung@gmail.com>
2023-12-08 19:30:14 +0800

    number validation

--- dist/formula.js
+++ dist/formula.js
@@ -60,3 +60,12 @@ function updateDOM() {
     document.getElementById("value4").innerHTML = calculate_Loan_StampDuty();
 }
 
+function onlyNumberKey(evt) {
+ 
+    // Only ASCII character in that range allowed
+    let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
