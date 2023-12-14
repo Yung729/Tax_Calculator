b6768868013f203fc4cfd961786bc1a582607406
Yung729 <leongkahyung@gmail.com>
2023-12-14 19:51:33 +0800

    a

--- dist/tenancyFormula.js
+++ dist/tenancyFormula.js
@@ -15,31 +15,32 @@ function tenancy_Days() {
     }
 }
 
-
-function calculate_StampDuty_Amount(){
-    let rentalRate = parseFloat(document.getElementById("rentalRate").value);
-    let remainder = Math.ceil((rentalRate*12-2400)/100)%100;
-    let quotient = Math.floor((rentalRate*12-2400)/1000)*1000;
-    let rate = tenancy_Days();
-    var amount;
-
-    if(remainder>75)
-        amount = quotient+1000;
-    else if(remainder>50)
-        amount = quotient+750;
-    else if(remainder>25)
-        amount = quotient+500;
-    else if(remainder>0)
-        amount = quotient+250;
+function nearest250(remainder,quotient) {
+    if (remainder > 75)
+        return quotient + 1000;
+    else if (remainder > 50)
+        return quotient + 750;
+    else if (remainder > 25)
+        return quotient + 500;
+    else if (remainder > 0)
+        return quotient + 250;
     else
-        amount = quotient;
-    
-    let stampDuty = amount/250 * rate;
-    return stampDuty;
+        return quotient;
+
 }
 
-function updateDOM(){
-    document.getElementById("demo").innerHTML = calculate_StampDuty_Amount();
+function calculate_TenancyFee_Amount() {
+    let rentalRate = parseFloat(document.getElementById("rentalRate").value);
+    let remainder = Math.ceil((rentalRate * 12 - 2400) / 100) % 100;
+    let quotient = Math.floor((rentalRate * 12 - 2400) / 1000) * 1000;
+    let rate = tenancy_Days();
+    var amount = nearest250(remainder,quotient);
+
+    return amount / 250 * rate;
+}
+
+function updateDOM() {
+    document.getElementById("demo").innerHTML = calculate_TenancyFee_Amount();
 }
 
 
