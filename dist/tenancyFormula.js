e5ff8f2e95b08552bfc30222cba6a73603b60bde
Yung729 <leongkahyung@gmail.com>
2023-12-14 21:16:35 +0800

    a

--- dist/tenancyFormula.js
+++ dist/tenancyFormula.js
@@ -43,5 +43,14 @@ function updateDOM() {
     document.getElementById("demo").innerHTML = calculate_TenancyFee_Amount();
 }
 
+function onlyNumberKey(evt) {
+
+    // Only ASCII character in that range allowed
+    let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
 
 
