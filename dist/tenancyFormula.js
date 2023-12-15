22dd8e6df9bc456c83324bc71d751601c93b2bd8
Yung <132829631+Yung729@users.noreply.github.com>
2023-12-15 14:27:34 +0800

    a

--- dist/tenancyFormula.js
+++ dist/tenancyFormula.js
@@ -16,21 +16,15 @@ function tenancy_Days() {
 }
 
 
-function calculate_StampDuty_Amount(){
-    let rentalRate = parseFloat(document.getElementById("rentalRate").value);
-    let remainder = (Math.ceil((rentalRate*12-2400)/100)*100)%1000;
-    let quotient = Math.floor((rentalRate*12-2400)/1000)*1000;
-    let rate = tenancy_Days();
-    var amount;
-
-    if(remainder>750)
-        amount = quotient+1000;
-    else if(remainder>500)
-        amount = quotient+750;
-    else if(remainder>250)
-        amount = quotient+500;
-    else if(remainder>0)
-        amount = quotient+250;
+function nearest250(remainder, quotient) {
+    if (remainder > 75)
+        return quotient + 1000;
+    else if (remainder > 50)
+        return quotient + 750;
+    else if (remainder > 25)
+        return quotient + 500;
+    else if (remainder > 0)
+        return quotient + 250;
     else
         return quotient;
 
@@ -38,16 +32,16 @@ function calculate_StampDuty_Amount(){
 
 function calculate_TenancyFee_Amount() {
     let rentalRate = parseFloat(document.getElementById("rentalRate").value);
-    let remainder = Math.ceil((rentalRate * 12 - 2400) / 100) % 100;
+    let remainder = (Math.ceil((rentalRate * 12 - 2400) / 100) * 100) % 1000;
     let quotient = Math.floor((rentalRate * 12 - 2400) / 1000) * 1000;
     let rate = tenancy_Days();
-    var amount = nearest250(remainder,quotient);
+    var amount = nearest250(remainder, quotient);
 
     return amount / 250 * rate;
 }
 
-function updateDOM() {
-    document.getElementById("demo").innerHTML = calculate_TenancyFee_Amount();
+function updateTenancyDOM() {
+    document.getElementById("result").innerHTML = calculate_TenancyFee_Amount();
 }
 
 function onlyNumberKey(evt) {
@@ -59,5 +53,12 @@ function onlyNumberKey(evt) {
     return true;
 }
 
-
+function showTenancyTable() {
+    var x = document.getElementById("tenancyRateContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
